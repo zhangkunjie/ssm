67504645594e2cced7e79b9ecbd16114a247ceee
kunjie.zhang <kunjie.zhang@corp.elong.com>
2018-05-28 22:01:24 +0800

    增删盖查

--- src/main/java/com/ssm/dao/BaseDao.java
+++ src/main/java/com/ssm/dao/BaseDao.java
@@ -1,6 +1,5 @@
 package com.ssm.dao;
 
-import com.ssm.model.PageModel;
 
 import java.util.List;
 
@@ -8,7 +7,61 @@ import java.util.List;
  * Created by kunjie.zhang on 2018/5/23.
  */
 public interface BaseDao<T extends java.io.Serializable, PK extends java.io.Serializable> {
-    public List<T> findAll(T t);
+    /**
+     * 保存实体
+     *
+     * @param entity
+     */
+    void insert(T entity);
 
-    public List<T> findPage(T t);
+    /*
+    * 批量插入
+    * @param entities
+    * */
+    void insertBatch(List<T> entities);
+
+    /**
+     * 删除实体
+     *
+     * @param entity
+     */
+    void delete(T entity);
+
+    /**
+     * 更新实体
+     *
+     * @param entity
+     */
+    void update(T entity);
+
+    /**
+     * 按id查询
+     *
+     * @param id
+     * @return
+     */
+    T getById(T t);
+
+    /**
+     * 按id查询
+     *
+     * @param ids
+     * @return
+     */
+    List<T> getByIds(PK[] ids);
+
+    /**
+     * 查询所有
+     *
+     * @return
+     */
+    List<T> findAll(T entity);
+
+    /*
+    *查询分页
+    * @return
+    */
+    List<T> findPage(T entity);
+
+
 }
